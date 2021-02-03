cf1668dfdbede7cba07f5bca7a05ab4b6be58527
ivanp7 <ivanp7@mail.ru>
2021-02-03 17:48:44 +0300

    Improve capture-video-segment.sh

--- helpers/capture-video-segment.sh
+++ helpers/capture-video-segment.sh
@@ -2,19 +2,18 @@
 
 if [ "$#" -eq 0 ]
 then
-    echo "capture-video-segment.sh URL FILENAME START_TIME DROP_FIRST_SECONDS RECORD_DURATION"
+    echo "capture-video-segment.sh URL FILENAME START_TIME END_TIME"
     exit 0
 fi
 
 URL="$1"
 FILENAME="$2"
 START_TIME="$3"
-DROP_FIRST_SECONDS="$4"
-RECORD_DURATION="$5"
+END_TIME="$4"
 
 youtube_urls=$(youtube-dl --youtube-skip-dash-manifest -g "$URL")
 video_url=$(echo "$youtube_urls" | head -1)
 audio_url=$(echo "$youtube_urls" | tail -1)
 
-ffmpeg -ss $START_TIME -i "$video_url" -ss $START_TIME -i "$audio_url" -map 0:v -map 1:a -ss $DROP_FIRST_SECONDS -t $RECORD_DURATION -c:v libx264 -c:a aac "$FILENAME"
+ffmpeg -ss "$START_TIME" -to "$END_TIME" -i "$video_url" -ss "$START_TIME" -to "$END_TIME" -i "$audio_url" -map 0:v -map 1:a -c:v libx264 -c:a aac "$FILENAME"
 
