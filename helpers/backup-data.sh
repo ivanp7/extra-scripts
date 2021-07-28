da916d3d7709acf26dc65be101c15f2e9308521a
ivanp7 <ivanp7@mail.ru>
2021-07-28 21:35:04 +0300

    Add Hitachi disk @ home-1 as a backup place

--- helpers/backup-data.sh
+++ helpers/backup-data.sh
@@ -20,35 +20,41 @@ backup ()
 
 ###############################################################################
 
-BACKUP_HOST="home-2"
+BACKUP_HOST_1="home-1"
+
+backup_hitachi ()
+{
+    local category="$1"
+    shift 1
+    backup "$category" "$BACKUP_HOST_1" hitachi "$@"
+}
+
+BACKUP_HOST_2="home-2"
 
 backup_seagate ()
 {
     local category="$1"
     shift 1
-    backup "$category" "$BACKUP_HOST" seagate "$@"
+    backup "$category" "$BACKUP_HOST_2" seagate "$@"
 }
 
 backup_wdc ()
 {
     local category="$1"
     shift 1
-    backup "$category" "$BACKUP_HOST" wdc "$@"
+    backup "$category" "$BACKUP_HOST_2" wdc "$@"
 }
 
 ###############################################################################
 ###############################################################################
 
-backup_seagate activities
-backup_seagate archive
-backup_seagate downloads
-backup_seagate encrypted-directories
-backup_seagate files
-backup_seagate git
-backup_seagate library
-backup_seagate media
-backup_seagate vm
-backup_seagate Windows
+backup_hitachi activities
+backup_hitachi archive
+backup_hitachi encrypted-directories
+backup_hitachi files
+backup_hitachi git
+backup_hitachi library
+backup_hitachi media
 
 backup_wdc     activities
 backup_wdc     archive
@@ -57,4 +63,16 @@ backup_wdc     files
 backup_wdc     git
 backup_wdc     library
 backup_wdc     media
+
+backup_seagate activities
+backup_seagate archive
+backup_seagate encrypted-directories
+backup_seagate files
+backup_seagate git
+backup_seagate library
+backup_seagate media
+
+backup_seagate vm
+backup_seagate Windows
+backup_seagate downloads
 
